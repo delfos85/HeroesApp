21e3dea48600e2e010bed181806e82c998485c7a
delfos85 <61164365+delfos85@users.noreply.github.com>
2020-03-31 15:23:21 -0600

    Actualizar, eliminar, listar y alertas

--- src/app/pages/heroe/heroe.component.ts
+++ src/app/pages/heroe/heroe.component.ts
@@ -3,6 +3,10 @@ import { HeroeModel } from '../../models/heroe.model';
 import { NgForm } from '@angular/forms';
 import { HeroesService } from '../../services/heroes.service';
 
+import Swal from 'sweetalert2';
+import { Observable } from 'rxjs';
+import { ActivatedRoute } from '@angular/router';
+
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
@@ -12,9 +16,21 @@ export class HeroeComponent implements OnInit {
 
   heroe = new HeroeModel();
 
-  constructor( private heroesService: HeroesService) { }
+  constructor( private heroesService: HeroesService, private route: ActivatedRoute) { }
 
   ngOnInit() {
+
+      const id = this.route.snapshot.paramMap.get('id');
+
+      if( id !== 'nuevo' ){
+
+        this.heroesService.getHeroe( id )
+        .subscribe( (resp: HeroeModel ) => {
+          this.heroe = resp;
+          this.heroe.id = id;
+        });
+      }
+
   }
 
   guardar( form: NgForm){
@@ -23,11 +39,39 @@ export class HeroeComponent implements OnInit {
       console.log('Formulario no valido');
       return;
     }
-    console.log(form);
-    this.heroesService.crearHeroe(this.heroe)
-      .subscribe(resp=>{
-        console.log(resp);
+
+    Swal.fire({
+      title: 'Espere',
+      text: 'Guardando información',
+      allowOutsideClick: false
+    });
+    Swal.showLoading();
+
+    let peticion: Observable<any>;
+
+    if( this.heroe.id ){
+      peticion = this.heroesService.actualizarHeroe( this.heroe );
+
+      peticion.subscribe( resp => {
+        Swal.fire({
+          title: this.heroe.nombre,
+          text: 'Se actualizo correctamente'
+        });
       });
+    }
+    else
+    {
+      peticion = this.heroesService.crearHeroe(this.heroe);
+
+      peticion.subscribe( resp => {
+        Swal.fire({
+          title: this.heroe.nombre,
+          text: 'Se registro correctamente'
+        });
+      });
+    }
+
+    
   }
 
 }
--- src/app/services/heroes.service.ts
+++ src/app/services/heroes.service.ts
@@ -12,7 +12,7 @@ export class HeroesService {
 
   constructor(private http: HttpClient) { }
 
-  crearHeroe( heroe:HeroeModel ){
+  crearHeroe( heroe: HeroeModel ){
     console.log(heroe);
     return this.http.post(`${this.url}/heroes.json`, heroe)
       .pipe(
@@ -21,5 +21,50 @@ export class HeroesService {
             return heroe;
         })
       );
+  }
+
+  actualizarHeroe( heroe: HeroeModel ){
+
+    const heroeTemp = {
+      ...heroe
+    };
+
+    delete heroeTemp.id;
+
+    return this.http.put(`${ this.url }/heroes/${ heroe.id }.json`, heroeTemp);
+  }
+
+  borrarHeroe( id: string ){
+
+    return this.http.delete(`${this.url}/heroes/${ id }.json`);
+  }
+
+  getHeroe( id: string){
+    return this.http.get(`${this.url}/heroes/${id}.json`);
+  }
+
+  getHeroes(){
+
+    return this.http.get(`${ this.url }/heroes.json`)
+            .pipe(
+              map( this.crearArreglo )
+            );
+  }
+
+  private crearArreglo( heroesObj: object){
+
+    const heroes: HeroeModel[] = [];
+
+    if( heroesObj === null ){ return []; }
+
+    Object.keys( heroesObj ).forEach( key => {
+      
+      const heroe: HeroeModel = heroesObj[key];
+      heroe.id = key;
+
+      heroes.push( heroe );
+    });
+
+    return heroes;
   }
 }
